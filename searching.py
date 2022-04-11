f53d16fd584155b7ae11929c10b8786d9f4d7419
Iveta Teturova <iveta.teturova@seznam.cz>
2022-04-11 09:24:25 +0200

    linear search function

--- searching.py
+++ searching.py
@@ -19,10 +19,21 @@ def read_data(file_name, field):
     return data[field]
 
 
+def linear_search(sequence, number):
+    slovnik = {"positions": [], "count": 0} #zadefinovani prazdneho slovniku s klicema
+    for index, num in enumerate(sequence):
+        if number == num:
+            slovnik["positions"].append(index)
+            slovnik["count"] = slovnik["count"] + 1
+    return slovnik
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
-
+    hledane_cislo = 5
+    results = linear_search(sequential_data, hledane_cislo)
+    print(results)
 
 if __name__ == '__main__':
     main()
